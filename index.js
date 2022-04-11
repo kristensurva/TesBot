31d2454b357cc40c2f0fc687f4b770787eb69b3c
Matteo Tarda <t_matteo@ymail.com>
2022-04-11 16:23:07 +0200

    - Added handling for incorrect prompt message author - Added data for thumbnail - Added fecthing embeds when there are no attachments

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('interactionCreate', async interaction => {
 		do {
 			messages = await interaction.channel.messages.fetch({ limit: 100, before: lastMessage });
 			lastMessage = messages?.last()?.id
-			filtered = messages.filter(message => message.reactions.cache.some(reaction => GALLERY_REACTIONS.includes(reaction.emoji.name)) || (message.author.id == '300246286580318209' && message.content.includes('prompts')));
+			filtered = messages.filter(message => message.reactions.cache.some(reaction => GALLERY_REACTIONS.includes(reaction.emoji.name)) || (message.author.id == '300246286580318209' && message.content.includes('prompts')) || ( message.author.id == '168034871724343296'  && message.content.includes('----------')));
 			all = all.concat(filtered);
 			console.log(all.size , 'messages collected')
 		}
@@ -63,8 +63,14 @@ client.on('interactionCreate', async interaction => {
 			break;
 		case 'gallery':
 			messageCollection = await _getAllMessages();
-			messageCollection = messageCollection.map(({ author, content, reactions, attachments }) => ({
-				content: author.id == '300246286580318209' ? content.split('\n')[3].slice(2, -2) : content, image: attachments?.first()?.url,
+			messageCollection = messageCollection.map(({ author, content, reactions, attachments, embeds }) => ({
+				content: author.id == '300246286580318209' || content.includes('----------') ? content.split('\n')[3].slice(2, -2) : content,
+				image: {
+					src: attachments?.first()?.url || embeds?.[0]?.url,
+					thumb: attachments?.first()?.proxyURL || embeds?.[0]?.thumbnail?.proxyURL,
+					width: attachments?.first()?.width || embeds?.[0]?.thumbnail?.width,
+					height: attachments?.first()?.height || embeds?.[0]?.thumbnail?.height
+				},
 				user: {
 					id: author.id,
 					name: author.username,
@@ -77,7 +83,7 @@ client.on('interactionCreate', async interaction => {
 			})).filter(({ user, content }) => user.id != '300246286580318209' || content).reverse();
 			reply = {};
 			for (let i = 0, currentDaily, currentWeekly, previousDaily, previousWeekly; i < messageCollection.length; i++) {
-				if (messageCollection[i].user.id == '300246286580318209') {
+				if (messageCollection[i].user.id == '300246286580318209' || messageCollection[i].content.includes('----------')) {
 					if (messageCollection[i].daily) {
 						previousDaily && (reply[previousDaily].entries.sort((a, b) => a.order - b.order));
 						previousDaily = currentDaily;
